d9bc7b49a64fd2b36e9cb92adf9f4fc8260a379d
Aftabb alamm <aftab1234011@gmail.com>
2020-05-24 16:52:26 -0400

    First Commit

---
+++ src/build/precache-manifest.ac0ed98a0eff0f8709e25b17dbe47baf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b962a8c686906b935d9f8add68edd65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "302a0b65e56538aba19a",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "7a4bd7e609d389213028",
+    "url": "/static/js/2.9178d880.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9178d880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "302a0b65e56538aba19a",
+    "url": "/static/js/main.e9a5b9bd.chunk.js"
+  },
+  {
+    "revision": "93ffc5734374f169bb64",
+    "url": "/static/js/runtime-main.121cda89.js"
+  }
+]);
